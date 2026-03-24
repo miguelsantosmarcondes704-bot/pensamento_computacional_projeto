898426d160f99619061a4d0041e251a83d963c8c
progamacao-cdt <miguelsantosmarcondes704@gmail.com>
2026-03-24 17:41:09 -0300

    Update projeto_app_Miguel.py

--- projeto_app_Miguel.py
+++ projeto_app_Miguel.py
@@ -32,15 +32,38 @@ while True:
     if escolha_menu == '1':
 
         print("Encomendando pedido...")
-        nome_cliente = input("Digite o nome do cliente:")
-        telefone_cliente = input("Digite o telefone do cliente: ")
+
+        nome_cliente = input("Digite o seu nome:")
+
+        telefone_cliente = input("Digite o seu telefone: ")
+
+        print ("Pedido a caminho,Obrigado pela preferência!")
 
 
-    elif escolha_menu == input '0' :
+    elif escolha_menu == input :
     
-        print("Saindo sistema. Até logo!")  
+        print("Saindo... Até logo!")  
     break
 
 
 else:
-    print("opção inválida.Por favor,tente novamente.")
+        print("opção inválida.Por favor,tente novamente.")
+
+        while true:
+
+
+          escolha_menu = input ("Digite o seu nome:")
+
+        if escolha_menu == '2':
+         
+
+         print ("Digite o seu endereço")
+
+         escolha_menu == input ("Bairro:")
+
+         escolha_menu == input ("Rua:")
+
+         escolha_menu == input ("Número da residência")
+
+         print ("Pedido a caminho,Obrigado pela preferência!")
+
